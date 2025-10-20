6c892df94a1540aea8b5dd08933f9293b2856512
cr0wBeak <134005427+cr0wBeak@users.noreply.github.com>
2025-10-20 01:43:06 -0700

    Added documentation comments

--- cr0wLib-cs/Logger.cs
+++ cr0wLib-cs/Logger.cs
@@ -8,8 +8,8 @@
     /// <param name="name">The name to be used in the Logger</param>
     public class Logger(Logger.LogLevel minSeverity, string? outDir, string name)
     {
-        private readonly LogLevel _minSeverity = minSeverity;
-        public readonly string? OutDir = outDir;
+        private readonly LogLevel _minimumSeverity = minSeverity;
+        public readonly string? OutputDirectory = outDir;
         public readonly string Name = name;
 
         /// <summary>
@@ -24,9 +24,16 @@
             Fatal
         }
 
+        /// <summary>
+        /// Logs the provided string to the console if the severity is at least
+        /// the Logger's minSeverity, then writes the Log message to a text file
+        /// if the output directory is not null.
+        /// </summary>
+        /// <param name="severity">The severity of the log entry</param>
+        /// <param name="entry">The string to be logged</param>
         private void Log(LogLevel severity, string entry)
         {
-            if (severity < _minSeverity)
+            if (severity < _minimumSeverity)
                 return;
 
             // format current time
@@ -36,18 +43,52 @@
 
             Console.WriteLine(message);
 
-            if (OutDir == null)
+            if (OutputDirectory == null)
                 return;
 
             // write message to file
             string fileName = $"{Name}-logs-{timestamp}.txt";
-            File.AppendAllText($"{OutDir}/{fileName}", $"{message}\n");
+            File.AppendAllText($"{OutputDirectory}/{fileName}", $"{message}\n");
         }
 
+        /// <summary>
+        /// Logs the provided string to the console if the Logger's minimum
+        /// severity is at least Debug level, then writes the Log message
+        /// to a text file if this.OutputDirectory is not null.
+        /// </summary>
+        /// <param name="entry">The string to be logged</param>
         public void Debug(string entry) => Log(LogLevel.Debug, entry);
+
+        /// <summary>
+        /// Logs the provided string to the console if the Logger's minimum
+        /// severity is at least Info level, then writes the Log message
+        /// to a text file if this.OutputDirectory is not null.
+        /// </summary>
+        /// <param name="entry">The string to be logged</param>
         public void Info(string entry) => Log(LogLevel.Info, entry);
+
+        /// <summary>
+        /// Logs the provided string to the console if the Logger's minimum
+        /// severity is at least Warning level, then writes the Log message
+        /// to a text file if this.OutputDirectory is not null.
+        /// </summary>
+        /// <param name="entry">The string to be logged</param>
         public void Warn(string entry) => Log(LogLevel.Warning, entry);
+
+        /// <summary>
+        /// Logs the provided string to the console if the Logger's minimum
+        /// severity is at least Error level, then writes the Log message
+        /// to a text file if this.OutputDirectory is not null.
+        /// </summary>
+        /// <param name="entry">The string to be logged</param>
         public void Error(string entry) => Log(LogLevel.Error, entry);
+
+        /// <summary>
+        /// Logs the provided string to the console if the Logger's minimum
+        /// severity is at least Fatal level, then writes the Log message
+        /// to a text file if this.OutputDirectory is not null.
+        /// </summary>
+        /// <param name="entry">The string to be logged</param>
         public void Fatal(string entry) => Log(LogLevel.Fatal, entry);
     }
 }
