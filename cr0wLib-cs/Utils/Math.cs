81c70d3f0fe1eff00b3d4c54ddc59739b2117a31
cr0wBeak <134005427+cr0wBeak@users.noreply.github.com>
2025-10-20 01:35:37 -0700

    Removed unnecessary dependencies

--- cr0wLib-cs/Utils/Math.cs
+++ cr0wLib-cs/Utils/Math.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace cr0wLib_cs.Utils
+﻿namespace cr0wLib_cs.Utils
 {
     public class Math
     {
