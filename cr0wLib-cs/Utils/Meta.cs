7ed18d798bc5a3db2a942fa205802929e396ba0e
cr0wBeak <134005427+cr0wBeak@users.noreply.github.com>
2025-10-20 19:41:20 -0700

    Added Utils.Meta

---
+++ cr0wLib-cs/Utils/Meta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cr0wLib_cs.Utils
+{
+    public static class Meta
+    {
+        public static TimeSpan TrackTime(Action f, int repetitions)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < repetitions; ++i)
+                f();
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+    }
+}
